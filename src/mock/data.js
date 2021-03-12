55b297faa0e369ada8aa9512b11cab0eff725cc8
KJEKcode <kurtiskovel@gmail.com>
2021-03-11 20:19:04 -0500

    favicon and settings

--- src/mock/data.js
+++ src/mock/data.js
@@ -59,7 +59,7 @@ export const projectsData = [
 export const contactData = {
   cta: '',
   btn: '',
-  email: 'kurtiskovel@gmail.com',
+  email: '',
 };
 
 // FOOTER DATA
@@ -67,23 +67,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/kurtis-kovel-10329b24',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'http://github.com/codeKJEK',
     },
   ],
 };
