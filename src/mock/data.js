6c18fa2a9b55b39cbbe870efceeff5e93693d149
KJEKcode <kurtiskovel@gmail.com>
2021-03-11 20:30:10 -0500

    full stack

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Kurtis Kovel',
-  subtitle: 'Web Designer & Developer',
+  subtitle: 'Full-Stack Developer',
   cta: '',
 };
 
